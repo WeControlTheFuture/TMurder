209bde3f3dc5f5fb39de687574e7d9ed7267d7f2
bixiyan <491676539@qq.com>
2018-04-04 17:36:31 +0800

    split

--- src/main/java/org/wctf/tool/murder/controller/BaseController.java
+++ src/main/java/org/wctf/tool/murder/controller/BaseController.java
@@ -8,7 +8,61 @@ public class BaseController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("call.......");
 		return "index";
+	}
+	
+	@RequestMapping("/table")
+	public String table() {
+		return "table";
+	}
+	
+	@RequestMapping("/ui")
+	public String ui() {
+		return "ui";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "form";
+	}
+	
+	@RequestMapping("/chart")
+	public String chart() {
+		return "chart";
+	}
+	
+	@RequestMapping("/typography")
+	public String typography() {
+		return "typography";
+	}
+	
+	@RequestMapping("/gallery")
+	public String gallery() {
+		return "gallery";
+	}
+	
+	@RequestMapping("/calendar")
+	public String calendar() {
+		return "calendar";
+	}
+	
+	@RequestMapping("/grid")
+	public String grid() {
+		return "grid";
+	}
+	
+	@RequestMapping("/icon")
+	public String icon() {
+		return "icon";
+	}
+	
+	@RequestMapping("/errorExample")
+	public String error() {
+		return "errorExample";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
