0e78a2d7a46c2b429716521132339a2b3e9909d5
bixy <491676539@qq.com>
2018-04-08 23:01:41 +0800

    change structure

---
+++ src/main/java/org/wctf/tool/murder/controller/QuartzContoller.java
@@ -0,0 +1,19 @@
+package org.wctf.tool.murder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("quartz")
+public class QuartzContoller {
+
+	@RequestMapping("/job-define")
+	public String jobdefine() {
+		return "quartz/job-define";
+	}
+	
+	@RequestMapping("/running-status")
+	public String runningstatus() {
+		return "quartz/running-status";
+	}
+}
